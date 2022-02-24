5725a73f1e5f8a8ecbcf3ce6560807a0e85ed472
unknown <ortegafernando334@gmail.com>
2022-02-23 21:04:19 -0500

    carrito compra v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const template = document.getElementById('template')
 const fragment = document.createDocumentFragment()
 const btnsBotones = document.querySelectorAll('.card .btn')
 
-const carritoObjeto = {}
+const carritoObjeto = [];
 
 const agregarAlCarrito = (e) => {
     console.log(e.target.dataset.fruta)
@@ -14,20 +14,24 @@ const agregarAlCarrito = (e) => {
         cantidad: 1,
     };
 
-    if(carritoObjeto.hasOwnProperty(producto.titulo)){
-        producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1
+    const indice = carritoObjeto.findIndex(
+        (item) => item.id === producto.id 
+    )
+
+    if (indice === -1) {
+        carritoObjeto.push(producto);
+    }else{
+        carritoObjeto[indice].cantidad++
     }
 
-    carritoObjeto[producto.titulo] = producto
-
-    pintarCarrito();
+    pintarCarrito(carritoObjeto);
 }
 
-const pintarCarrito = () => {
+const pintarCarrito = (array) => {
 
     carrito.textContent = ""
 
-    Object.values(carritoObjeto).forEach((item) => {
+    array.forEach((item) => {
         const clone = template.content.firstElementChild.cloneNode(true)
         clone.querySelector(".lead").textContent = item.titulo
         clone.querySelector(".badge").textContent = item.cantidad
