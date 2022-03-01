48295dd2576ae930925581ddf06ab2ed44d23466
unknown <ortegafernando334@gmail.com>
2022-02-28 20:05:26 -0500

    carrito compra v3

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const carrito = document.getElementById('carrito')
 const template = document.getElementById('template')
+const footer = document.getElementById('footer')
+const templateFooter = document.getElementById('templateFooter')
 const fragment = document.createDocumentFragment()
-const btnsBotones = document.querySelectorAll('.card .btn')
 
-const carritoObjeto = [];
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".card .btn-outline-primary")) {
+        agregarAlCarrito(e)
+    }
+    if(e.target.matches(".list-group-item .btn-success")){
+        btnAumentar(e);
+    }
+    if(e.target.matches(".list-group-item .btn-danger")){
+        btnDisminuir(e);
+    }
+})
+
+let carritoObjeto = [];
 
 const agregarAlCarrito = (e) => {
-    console.log(e.target.dataset.fruta)
 
     const producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
         cantidad: 1,
+        precio: parseInt(e.target.dataset.precio)
     };
 
     const indice = carritoObjeto.findIndex(
@@ -21,25 +34,68 @@ const agregarAlCarrito = (e) => {
     if (indice === -1) {
         carritoObjeto.push(producto);
     }else{
-        carritoObjeto[indice].cantidad++
+        carritoObjeto[indice].cantidad++;
+        // carritoObjeto[indice].precio = carritoObjeto[indice].cantidad * producto.precio
     }
 
-    pintarCarrito(carritoObjeto);
+    console.log(carritoObjeto)
+    pintarCarrito();
 }
 
-const pintarCarrito = (array) => {
+const pintarCarrito = () => {
 
     carrito.textContent = ""
 
-    array.forEach((item) => {
-        const clone = template.content.firstElementChild.cloneNode(true)
+    carritoObjeto.forEach((item) => {
+        const clone = template.content.cloneNode(true)
         clone.querySelector(".lead").textContent = item.titulo
         clone.querySelector(".badge").textContent = item.cantidad
-
+        clone.querySelector('div .lead span').textContent = item.precio * item.cantidad
+        clone.querySelector('.btn-danger').dataset.id = item.id
+        clone.querySelector('.btn-success').dataset.id = item.id
         fragment.appendChild(clone);
     });
 
     carrito.appendChild(fragment)
+
+    pintarFooter();
 }
 
-btnsBotones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito))
+const pintarFooter = () => {
+    footer.textContent = ""
+    const total = carritoObjeto.reduce(
+        (acc, currente) => acc + currente.cantidad * currente.precio, 0
+    )
+
+    const clone = templateFooter.content.cloneNode(true)
+    clone.querySelector("span").textContent = total
+    footer.appendChild(clone)
+}
+
+const btnAumentar = (e) => {
+    carritoObjeto = carritoObjeto.map(item => {
+        if(item.id === e.target.dataset.id){
+            item.cantidad ++
+        }
+        return item
+    })
+
+    pintarCarrito()
+}
+
+const btnDisminuir = (e) => {
+    carritoObjeto = carritoObjeto.filter(item => {
+        if(item.id === e.target.dataset.id){
+            if (item.cantidad > 0) {
+                item.cantidad --   
+                if (item.cantidad === 0) return 
+                return item      
+            }
+        }else{
+            return item
+        }
+    })
+
+    pintarCarrito()
+}
+
